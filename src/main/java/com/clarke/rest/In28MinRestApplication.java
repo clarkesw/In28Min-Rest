f4a31e1021d60b35fbdbb2f3f931fca63cdf8e8c
clarkesw <clarke.job@gmail.com>
2024-09-13 16:45:46 -0400

    First Round

---
+++ src/main/java/com/clarke/rest/In28MinRestApplication.java
@@ -0,0 +1,13 @@
+package com.clarke.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class In28MinRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(In28MinRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clarke/rest/In28MinRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clarke.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class In28MinRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
