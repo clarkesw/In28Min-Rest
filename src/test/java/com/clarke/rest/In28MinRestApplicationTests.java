0e5041dd0777da4c015bd0a6a73a9d144b05ca5f
clarkesw <clarke.job@gmail.com>
2024-09-17 15:06:11 -0400

    Remove Test

--- src/test/java/com/clarke/rest/In28MinRestApplicationTests.java
+++ src/test/java/com/clarke/rest/In28MinRestApplicationTests.java
@@ -3,11 +3,11 @@ package com.clarke.rest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class In28MinRestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
