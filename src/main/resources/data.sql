bc00f100000dc67f8b5dc75c6fe00f0870602340
clarke <clarke@clarke-HP-Laptop-14-dq0xxx>
2024-09-17 15:37:04 -0400

    data initialization fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 insert into user_details
 values('1999-02-09',1,'Tad');
 insert into user_details
-values('1923-12-09',1,'Meogi');
+values('1923-12-09',2,'Meogi');
 insert into user_details
-values('1977-02-09',1,'John');
+values('1977-02-09',3,'John');
